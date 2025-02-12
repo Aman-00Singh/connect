2e99a4d65d466f430d2ecc658cc8d5acf372b047
Aman-00Singh <amansingh95336@gmail.com>
2025-02-13 04:48:47 +0530

    Add initial layout and page components for home, upcoming, previous, recordings, and personal room

---
+++ constants/index.ts
@@ -0,0 +1,32 @@
+export const sidebarLinks = [
+    {   
+        label : "Home",
+        imageUrl :"/icons/Home.svg",
+        route : "/"
+    },
+
+    {   
+        label : "Upcoming",
+        imageUrl :"/icons/upcoming.svg",
+        route : "/upcoming"
+    },
+
+    {   
+        label : "Previous",
+        imageUrl :"/icons/previous.svg",
+        route : "/previous"
+    },
+
+    {   
+        label : "Recordings",
+        imageUrl :"/icons/Video.svg",
+        route : "/recordings"
+    },
+
+    
+    {   
+        label : "Personal-room",
+        imageUrl :"/icons/add-personal.svg",
+        route : "/personal-room"
+    },
+]
