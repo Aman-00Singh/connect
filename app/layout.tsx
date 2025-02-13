7dfc6d5541644a229483e5980fd3edeb7b7fa325
Aman-00Singh <amansingh95336@gmail.com>
2025-02-14 04:30:42 +0530

    Add authentication pages and middleware with Clerk integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,12 +24,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider 
+      dynamic
+      afterSignOutUrl="/sign-in"
+         appearance={{
+          layout: {
+            socialButtonsVariant: "iconButton",
+            logoImageUrl: "/icons/Video.svg",
+          },
+          variables: {
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1C1F2E",
+            colorInputBackground: "#252A41",
+            colorInputText: "#fff",
+          },
+        }}
+    >
+      <html lang="en">   
       <body
         className={`${geistSans.variable} ${geistMono.variable} bg-dark-1`}
       >
         {children}
       </body>
     </html>
+    </ClerkProvider>
+    
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,13 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import MobileNav from './MobileNav';
-// import { SignedIn, UserButton } from '@clerk/nextjs';
+import { SignedIn, UserButton } from '@clerk/nextjs';
 
 
 
 const Navbar = () => {
   return (
-    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10">
+    <nav className="flex-between fixed z-50 w-full bg-dark-2 px-6 py-4 lg:px-10">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/icons/logo.svg"
@@ -21,9 +21,10 @@ const Navbar = () => {
         </p>
       </Link>
       <div className="flex-between gap-5">
-        {/* <SignedIn>
-          <UserButton afterSignOutUrl="/sign-in" /> */}
-        {/* </SignedIn> */}
+        <SignedIn>
+          <UserButton
+          />
+        </SignedIn>
 
         <MobileNav/>
       </div>
